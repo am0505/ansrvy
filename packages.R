956b45fade0b039fd0793268cff3db4f8c8750cf
am0505 <amotheram@ncaer.org>
2023-07-11 20:08:44 +0530

    Packages needed for the project

---
+++ packages.R
@@ -0,0 +1,24 @@
+## library() calls go here
+#library(conflicted)
+library(dotenv)
+library(targets)
+library(tarchetypes)
+
+library(tidyverse)
+library(janitor)
+library(readxl)
+library(haven)
+library(sjlabelled)
+library(sjmisc)
+library(glue)
+library(here)
+
+library(srvyr)
+
+#library(countrycode)
+
+#conflict_prefer("filter", "dplyr")
+#conflict_prefer("lag", "dplyr")
+#conflict_prefer("cols", "vroom")
+
+theme_set(theme_minimal())
